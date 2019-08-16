3a6a79895596a189b2465c139689c80a22b96a86
1563501626 <hkxy_hujie.163.com>
2019-08-16 13:40:59 +0800

    0816

--- learn.py
+++ learn.py
@@ -1,12 +1,22 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.datasets import fetch_20newsgroups
+from sklearn.datasets import fetch_20newsgroups, load_boston
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.metrics import mean_squared_error, classification_report
+from sklearn.linear_model import LogisticRegression
+
+pd.set_option('display.max_columns', None)
+# 显示所有行
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 1000)
 
 
 def kneighbors():
@@ -72,7 +82,7 @@ def decision():
     决策树
     :return:
     """
-    df = pd.read_csv("titanic.csv")
+    df = pd.read_csv(r"C:\Users\Administrator\Desktop\titanic.txt")
     x = df.loc[:, ['pclass', 'age', 'sex']]
     y = df.loc[:, 'survived']
     x.age = x.age.fillna(x.age.mean())
@@ -82,10 +92,90 @@ def decision():
     print(x_train, '\n', dicts.get_feature_names())
     x_test = dicts.transform(x_test.to_dict(orient='record'))
     # 决策树
-    deci = DecisionTreeClassifier(max_depth=5)
+    deci = DecisionTreeClassifier()
     deci.fit(x_train, y_train)
     print(deci.score(x_test, y_test))
 
 
+def forest():
+    """
+    随机森林
+    :return:
+    """
+    df = pd.read_csv(r"C:\Users\Administrator\Desktop\titanic.txt")
+    x = df.loc[:, ['pclass', 'age', 'sex']]
+    y = df.loc[:, 'survived']
+    x.age = x.age.fillna(x.age.mean())
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+    dicts = DictVectorizer(sparse=False)
+    x_train = dicts.fit_transform(x_train.to_dict(orient='record'))
+    print(x_train, '\n', dicts.get_feature_names())
+    x_test = dicts.transform(x_test.to_dict(orient='record'))
+    # 随机森林 (超参数调优) n_estimators决策树的最大棵数
+    rf = RandomForestClassifier()
+    # 网格搜索和交叉验证
+    params = {"n_estimators": [10,200,300,500,800,1200], 'max_depth': [5,8,15,25,30]}
+    gc = GridSearchCV(rf, param_grid=params)
+    gc.fit(x_train, y_train)
+    print(gc.score(x_test, y_test))
+    print('最佳参数模型为', gc.best_params_)
+
+
+def lineas():
+    """
+    线性回归
+    :return:
+    """
+    lb = load_boston()
+    x_train, x_test, y_train, y_test= train_test_split(lb.data, lb.target, test_size=0.25)
+    # 标准化处理
+    std_x = StandardScaler()
+    x_train = std_x.fit_transform(x_train)
+    x_test = std_x.transform(x_test)
+    std_y = StandardScaler()
+    y_train = std_y.fit_transform(y_train.reshape(-1, 1))  # 需要将一维数组转换为二维数组
+    y_test = std_y.transform(y_test.reshape(-1, 1))
+    # 线性回归
+    # 正规方程
+    lr = LinearRegression()
+    lr.fit(x_train, y_train)
+    y_pre = lr.predict(x_test)
+    y_pre = std_y.inverse_transform(y_pre)
+    # print("result", y_pre)
+    # print("权重", lr.coef_)
+    print("均方误差1", mean_squared_error(std_y.inverse_transform(y_test), y_pre))
+
+    # 梯度下降
+    sgd = SGDRegressor()
+    sgd.fit(x_train, y_train)
+    y_pre2 = std_y.inverse_transform(sgd.predict(x_test))
+    # print('权重', sgd.coef_)
+    print("均方误差2", mean_squared_error(std_y.inverse_transform(y_test), y_pre2))
+
+
+def logistic():
+    """
+    逻辑回归
+    :return:
+    """
+    columns = ['Sample code number', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size',
+               'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
+    data = pd.read_csv('D:\crawl_datasource\cancer.data', names=columns)
+    data = data.replace(to_replace=['?'], value=np.nan)
+    data = data.dropna()
+    x_train, x_test, y_train, y_test = train_test_split(data[columns[1:-1]], data[columns[-1]])
+    # 标准化处理
+    std = StandardScaler()
+    x_train = std.fit_transform(x_train)
+    x_test = std.transform(x_test)
+    # 逻辑回归
+    lg = LogisticRegression(penalty='l2', C=1.0)
+    lg.fit(x_train, y_train)
+    y_pre = lg.predict(x_test)
+    print(y_pre)
+    print("准确率", lg.score(x_test, y_test))
+    print("召回率", classification_report(np.array(y_test).reshape(-1, 1), y_pre.reshape(-1, 1), labels=[2, 4], target_names=['良性', '恶性']))
+
+
 if __name__ == '__main__':
-    decision()
+    logistic()
